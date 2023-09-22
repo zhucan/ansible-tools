5f3abd2ea37211d109aaaf89d89d78b58952a415
canzhu <zhucan.k8s@gmail.com>
2023-09-22 15:29:59 +0800

    install hangs
    Signed-off-by: canzhu <zhucan.k8s@gmail.com>

--- prepare.sh
+++ prepare.sh
@@ -6,6 +6,9 @@ sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
 # install
+wget https://rpmfind.net/linux/centos/8-stream/AppStream/x86_64/os/Packages/sshpass-1.09-4.el8.x86_64.rpm
+rpm -Uvh sshpass-1.09-4.el8.x86_64.rpm --force
+rm -f sshpass-1.09-4.el8.x86_64.rpm
 dnf install epel-release -y
 dnf install python3 python3-pip -y
 dnf install rsync -y 
